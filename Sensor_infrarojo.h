34f288c2aec55daaa1c6e2589ad9a5c10ad32981
Cris153 <cristiancravero023@gmail.com>
2018-04-07 08:52:27 -0300

    Add files via upload

---
+++ Sensor_infrarojo.h
@@ -0,0 +1,21 @@
+class Infrarrojo
+{
+  public:
+  Infrarrojo(int pin, int blanco);
+  bool leeBlanco();
+  
+  private:
+  int _pin;
+  int _blanco;
+};
+
+Infrarrojo::Infrarrojo(int pin, int blanco): _pin(pin), _blanco(blanco) {
+  pinMode(_pin,INPUT);
+}
+
+bool Infrarrojo::leeBlanco()
+{
+  if(analogRead(_pin) >= _blanco)
+    return true;
+  return false;  
+}
---
+++ Sensor_infrarojo.ino
@@ -0,0 +1,35 @@
+class Infrarrojo
+{
+  public:
+  Infrarrojo(int pin, int blanco);
+  bool leeBlanco();
+  
+  private:
+  int _pin;
+  int _blanco;
+};
+
+Infrarrojo::Infrarrojo(int pin, int blanco): _pin(pin), _blanco(blanco) {
+  pinMode(_pin,INPUT);
+}
+
+bool Infrarrojo::leeBlanco()
+{
+  if(analogRead(_pin) >= blanco)
+    return true;
+  return false;  
+}
+
+class robot {
+  private:
+  
+  
+  
+  
+  public:
+  
+  
+  
+    
+  
+  };
